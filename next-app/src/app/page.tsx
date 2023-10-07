7d4d6c30b6c667e0bbbb455a1ad1bfecac731275
e5pe0n <1izard.synchr0fi5h@gmail.com>
2023-10-07 20:33:16 +0900

    feat: chat messages

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,12 +1,107 @@
+"use client";
+import { useEffect, useState } from "react";
+import { socket } from "@/socket";
 import { Button } from "@nextui-org/button";
-import { Chip } from "@nextui-org/chip";
+import { Input } from "@nextui-org/input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const useSocketConnection = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    const onConnect = () => {
+      setIsConnected(true);
+    };
+
+    const onDisconnect = () => {
+      setIsConnected(false);
+    };
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
+  return { isConnected };
+};
+
+const useMessages = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const onReceiveMessage = (message: string) => {
+      setMessages((v) => [...v, message]);
+    };
+
+    socket.on("chat message", onReceiveMessage);
+
+    return () => {
+      socket.off("chat message", onReceiveMessage);
+    };
+  }, []);
+
+  return { messages };
+};
+
+type FormValues = {
+  message: string;
+};
 
 export default function Home() {
+  const { isConnected } = useSocketConnection();
+  const { messages } = useMessages();
+  const { register, handleSubmit, reset } = useForm<FormValues>({
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = ({ message }) => {
+    socket.emit("chat message", message);
+    reset({ message: "" });
+  };
+
+  const onConnectClick = () => {
+    socket.connect();
+  };
+
+  const onDisconnectClick = () => {
+    socket.disconnect();
+  };
+
   return (
     <main className="min-h-screen p-24">
       <div>
-        <Button color="primary">Button</Button>
-        <Chip color="primary">Chip</Chip>
+        <p>State: {`${isConnected}`}</p>
+        <Button color="primary" onClick={onConnectClick}>
+          Connect
+        </Button>
+        <Button color="danger" onClick={onDisconnectClick}>
+          Disconnect
+        </Button>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex items-center gap-2">
+            <Input
+              {...register("message")}
+              label="message"
+              variant="underlined"
+            />
+            <Button color="primary" type="submit">
+              Send
+            </Button>
+          </div>
+        </form>
+        <ul>
+          {messages.map((message, i) => (
+            <li key={i}>{message}</li>
+          ))}
+        </ul>
       </div>
     </main>
   );
--- socket-server/main.ts
+++ socket-server/main.ts
@@ -1,1 +1,28 @@
-export {};
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnectted");
+  });
+
+  socket.on("chat message", (message: string) => {
+    console.log("message:", message);
+    io.emit("chat message", message);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server runnig at http://localhost:3001");
+});
